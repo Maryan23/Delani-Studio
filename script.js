6c729780093714526b67fc80101381ba3587463b
Maryann Mwikali <maryann.mwikali@student.moringaschool.com>
2021-09-14 00:54:19 +0300

    Add form validation function

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ $(document).ready(function(){
         $("#product").toggle();
     });
 });
+function feedBack(){
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var message=document.getElementById('message').value;
+    alert(name)
+
+    var regEx=/^[A-Za-z]+$/;
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if ((name.value.match(regEx)) && (email.value.match(validRegex)) && (message.value.match(regEx))){
+        alert("Hi "+" name "+" We have received your message. Thank you for contacting Delani Studio. ")
+    }else {
+        alert("Please check your input!")
+    }
+}
