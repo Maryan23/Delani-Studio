3fadc570a70227b5cf7a0d6703204ee7693cbfeb
Maryann Mwikali <maryann.mwikali@student.moringaschool.com>
2021-09-13 11:34:29 +0300

    introduce toggle section

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+        $(".design").toggle();
+        $("#design").toggle();
+    })
+    $("#development").click(function(){
+        $(".development").toggle();
+        $("#development").toggle();
+    });
+    $("#product").click(function(){
+        $(".product").toggle();
+        $("#product").toggle();
+    });
+});
