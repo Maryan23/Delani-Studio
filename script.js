a4f3240c086a6f46ac911f64bcb9d66f19e92ef7
Maryann Mwikali <maryann.mwikali@student.moringaschool.com>
2021-09-15 10:13:12 +0300

    add toggle

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
 $(document).ready(function(){
     $("#design").click(function(){
+        $("#des").hide();
         $(".design").toggle();
         $("#design").toggle();
-    })
+     });
     $("#development").click(function(){
+        $("#dev").hide();
         $(".development").toggle();
         $("#development").toggle();
     });
     $("#product").click(function(){
+        $("#mgt").hide();
         $(".product").toggle();
         $("#product").toggle();
     });
 });
-function feedBack(){
-    var name=document.getElementById('name').value;
-    var email=document.getElementById('email').value;
-    var message=document.getElementById('message').value;
-    alert(name)
-
-    var regEx=/^[A-Za-z]+$/;
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if ((name.value.match(regEx)) && (email.value.match(validRegex)) && (message.value.match(regEx))){
-        alert("Hi "+" name "+" We have received your message. Thank you for contacting Delani Studio. ")
-    }else {
-        alert("Please check your input!")
-    }
-}
